f971af2dfa374a9fc026d6e1a67a03f4f8c21531
Huynh Binh <huynhbinh0511@gmail.com>
2018-06-29 00:44:28 +0700

    update test case

--- BitLottery/test/TestLotteryContract.sol
+++ BitLottery/test/TestLotteryContract.sol
@@ -7,7 +7,7 @@ import "../contracts/Clock.sol";
 
 contract TestLotteryContract
 {
-    uint public initialBalance = 10 ether; 
+    uint public initialBalance = 10 ether;
 
     function begin() private returns(LotteryContract, Clock, Random)
     {
@@ -26,7 +26,7 @@ contract TestLotteryContract
 
         (lot, clock, random) = begin();
 
-        uint expected = 100000;
+        uint expected = 99999;
         uint rand = lot._findWinningNumber();
 
         Assert.isAbove(rand, expected, "Random number must be > 100.000");
@@ -63,8 +63,72 @@ contract TestLotteryContract
         lot.startNewCampaign();
         Assert.equal(lot._isCampaignEnd(), false, "Campaign must be in OPEN state.");
 
-        clock.setNow(now + 3 days);
+        uint duration = 3 days;
+        clock.setNow(now + duration);
         Assert.equal(lot._isCampaignEnd(), true, "Campaign must be in END state.");
     }
+
+    function testTicketPrice() public
+    {
+        LotteryContract lot;
+        Clock clock;
+        Random random;
+
+        (lot, clock, random) = begin();
+
+        Assert.equal(lot.campaignTicketPrice(), 0.01 ether, "Ticket price is not correct.");
+        Assert.equal(lot.actualTicketPrice(), 0.009 ether, "Ticket price is not correct.");
+
+        lot.updateTicketPrice(10 ether, 9 ether);
+        Assert.equal(lot.campaignTicketPrice(), 10 ether, "Ticket price is not correct.");
+        Assert.equal(lot.actualTicketPrice(), 9 ether, "Ticket price is not correct.");
+
+        lot.updateTicketPrice(1 ether, 0.9 ether);
+        Assert.equal(lot.campaignTicketPrice(), 1 ether, "Ticket price is not correct.");
+        Assert.equal(lot.actualTicketPrice(), 0.9 ether, "Ticket price is not correct.");
+
+    }
+
+    function testCampaignDuration() public
+    {
+        LotteryContract lot;
+        Clock clock;
+        Random random;
+
+        (lot, clock, random) = begin();
+
+        Assert.equal(lot.campaignDuration(), 3 days, "TDuration must be 3 days.");
+
+        lot.updateCampaignDuration(10 days);
+        Assert.equal(lot.campaignDuration(), 10 days, "TDuration must be 10 days.");
+
+        lot.updateCampaignDuration(1 days);
+        Assert.equal(lot.campaignDuration(), 1 days, "TDuration must be 10 days.");
+
+        lot.updateCampaignDuration(0 days);
+        Assert.equal(lot.campaignDuration(), 0 days, "TDuration must be 10 days.");
+    }
+
+
+    function testFallback() public 
+    {
+        LotteryContract lot;
+        Clock clock;
+        Random random;
+
+        (lot, clock, random) = begin();
+
+        Assert.equal(address(lot).balance, 0, "Not equal");
+
+        address(lot).transfer(5000)();
+        //Assert.equal(address(lot).balance, 5000 wei, "Not equal");
+    }
+
+
+
+
+
+
+
 
 }
